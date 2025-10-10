6a2ed0963626461385690991b854681d99e69495
ThomasTomanec <tomasttomanec@gmail.com>
2025-10-11 01:35:58 +0200

    update

--- src/utils/async_helpers.py
+++
@@ -1,9 +0,0 @@
-"""Async utilities"""
-import asyncio
-from typing import Callable, Any
-
-
-async def run_in_thread(func: Callable, *args, **kwargs) -> Any:
-    """Run blocking function in thread pool"""
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, lambda: func(*args, **kwargs))
